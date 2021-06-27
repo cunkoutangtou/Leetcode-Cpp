adf8173ee70f5ef07d702fdfbaa17c4188629fa3
cunkoutangtou <57053383+cunkoutangtou@users.noreply.github.com>
2021-06-27 22:08:05 +0800

    Create 旋转图像.cpp

---
+++ 二维数组/旋转图像.cpp
@@ -0,0 +1,60 @@
+给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+
+你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+
+ 
+
+示例 1：
+
+
+输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+输出：[[7,4,1],[8,5,2],[9,6,3]]
+示例 2：
+
+
+输入：matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+输出：[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+示例 3：
+
+输入：matrix = [[1]]
+输出：[[1]]
+示例 4：
+
+输入：matrix = [[1,2],[3,4]]
+输出：[[3,1],[4,2]]
+
+链接：https://leetcode-cn.com/problems/rotate-image
+
+//将图像分为四个部分，每次交换四个位置的值，注意循环中对边长为奇数和偶数的处理
+class Solution {
+public:  
+  void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int i=0;i<n/2;i++){//对奇偶取同值，比如4,5除2都是2。
+            for(int j=0;j<(n+1)/2;j++){//遇奇数加一个值，比如5+1,6+1除2都是3。
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[n-j-1][i];
+                matrix[n-j-1][i]=matrix[n-i-1][n-j-1];
+                matrix[n-i-1][n-j-1]=matrix[j][n-i-1];
+                matrix[j][n-i-1]=temp;
+            }
+        }
+     }
+}；
+//先水平翻转，再对角线翻转
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int i=0;i<n/2;i++){
+            for(int j=0;j<n;j++){
+                swap(matrix[i][j],matrix[n-1-i][j]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+    }
+};
