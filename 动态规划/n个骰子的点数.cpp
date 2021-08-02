fd77131c03b16131f70a1b711148c6fa3dea0b1b
cunkoutangtou <57053383+cunkoutangtou@users.noreply.github.com>
2021-08-02 19:11:51 +0800

    Update n个骰子的点数.cpp

--- 动态规划/n个骰子的点数.cpp
+++ 动态规划/n个骰子的点数.cpp
@@ -22,13 +22,13 @@
 
 链接：https://leetcode-cn.com/problems/nge-tou-zi-de-dian-shu-lcof
 
-
+//前向累加，将上一轮的结果逐渐向后延伸直到满足骰子的最大能组成的数。
 class Solution {
 public:
     vector<double> dicesProbability(int n) {
         vector<double> dp(6,1.0/6);
         for(int i=2;i<=n;i++){
-            vector<double> tmp(5*i+1,0);
+            vector<double> tmp(5*i+1,0);//这一轮会有6i-(i-1)个数产生。
             for(int j=0;j<dp.size();j++){
                 for(int k=0;k<6;k++){
                     tmp[j+k]+=dp[j]/6;
@@ -37,5 +37,31 @@ public:
             dp=tmp;
         }
         return dp;
+    }
+};
+//从后向前算，需要注意骰子个数少一时的最小数为边界。
+class Solution {
+    int maxvalue=6;
+public:
+    vector<double> dicesProbability(int n) {
+        vector<int> dp(maxvalue*n+1,0);
+        for(int i=1;i<=maxvalue;i++)
+        dp[i]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=maxvalue*i;j>=i;j--){
+                dp[j]=0;//每一轮第j个位置要重新计算，否则就会有重复计算累加
+                for(int k=1;k<=maxvalue;k++){
+                    if(j-k<i-1)//骰子个数为i-1时的最小能组成的数为i-1
+                    break;
+                    dp[j]+=dp[j-k];//第j个位置能由哪些数字组成
+                }
+            }
+        }
+        vector<double> re;
+        int total=pow(maxvalue,n);
+        for(int i=n;i<=maxvalue*n;i++){
+            re.push_back(1.0*dp[i]/total);
+        }
+        return re;
     }
 };
